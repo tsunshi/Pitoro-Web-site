ebce1eaac1285b85c11142ae3c95b388f88cb027
tsunshi <57198088+tsunshi@users.noreply.github.com>
2020-02-20 23:50:04 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/websaite#">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="google-site-verification" content="ttc9t6p-ucA4PsEdEyqwvXCwzKCbuiUfwq9msDupKUI" />
+    <meta name="description" content="京都の革職人「Pitoro」。味わい深いレザー小物の販売や、オーダーメイド制作も承ります。">
+    <meta property="og:url" content="http://tsunshi283.php.xdomain.jp/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="レザー小物とオーダーメイド" />
+    <meta property="og:description" content="京都の革職人「Pitoro」。味わい深いレザー小物の販売や、オーダーメイド制作も承ります。" />
+    <meta property="og:site_name" content="京都の革職人 Pitoro" />
+    <meta property="og:image" content="http://tsunshi283.php.xdomain.jp/img/ogm.png" />
+    <!--twitterの設定-->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="http://tsunshi283.php.xdomain.jp/">
+    <meta name="twitter:image" content="http://tsunshi283.php.xdomain.jp/img/ogm.png" />
+    <meta name="twitter:title" content="京都の革職人 Pitoro">
+    <meta name="twitter:description" content="京都の革職人「Pitoro」。味わい深いレザー小物の販売や、オーダーメイド制作も承ります。">
+
+
+    <!-- iScroll -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.1.3/iscroll.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="/js/scrolla.jquery.js"></script>
+    <script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
+    <link rel="icon" type="image/x-icon" href="img/favicon.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/ogm.png">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.css">
+    <title>京都の革職人 Pitoro</title>
+
+</head>
+<body>
+    <div class="loader">
+        <img class="loader-bg" src="img/loading.gif">
+        <p class="loader-text">Loading・・・</p>
+    </div>
+    <aside>
+        <div id="navArea">
+            <nav>
+                <div class="nav__inner">
+                <ul class="nav__inner__list">
+                    <li class="nav__inner__list__item"><a href="#top"><span class="nomal">TOP</span><span  class="hover">トップ</span></a></li>
+                    <li class="nav__inner__list__item"><a href="#about"><span class="nomal">ABOUT</span><span  class="hover">Pitoroについて</span></a></li>
+                    <li class="nav__inner__list__item"><a href="#order"><span class="nomal">FULL ORDER MADE</span><span  class="hover">オーダーメイド</span></a></li>
+                    <li class="nav__inner__list__item"><a href="#backSide"><span class="nomal">THE BACK SIDE</span><span  class="hover">ものづくりの裏側</span></a></li>
+                    <li class="nav__inner__list__item"><a href="#message"><span class="nomal">MESSAGE</span><span  class="hover">メッセージ</span></a></li>
+                    <li class="nav__inner__list__item"><a href="#contact"><span class="nomal">CONTACT</span><span  class="hover">お問い合わせ</span></a></li>
+                </ul>
+                </div>
+            </nav>          
+            <div class="toggle_btn">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>         
+            <div id="mask"></div>         
+        </div>
+        <div class="link">
+            <ul class="link__inner">
+                <li class="link__inner__item"><a href="https://pitoro.base.shop/" target="_blank"><img class="link-icon" src="img/shop.png" alt="ショップリンク"></a></li>
+                <li class="link__inner__item"><a href="https://twitter.com/Pitoroleather" target="_blank"><img class="link-icon" src="img/twitter.png" alt="twitter"></a></li>
+                <li class="link__inner__item"><a href="https://www.instagram.com/pitorokatsuo/?hl=ja" target="_blank"><img class="link-icon" src="img/instagram.png" alt="instagram"></a></li>
+            </ul>
+        </div>
+        <div class="to-top">
+            <img class="to-top-btn" src="/img/top_btn.png" alt="トップへ戻る">
+        </div>
+    </aside>
+    <header id="top">
+        <h1 class="header__logo"><img class="logo-white" src="/img/logo_white.png" alt="革職人 Pitoro"></h1>
+        <p class="scroll-arrow"><img src="/img/scroll_arrow.png" alt="下にスクロール"></p>
+    </header>
+    <main role="main">
+        <article class="column flex animate" data-animate="fadeIn" data-duration="2.0s" data-delay="0.5s" data-offset="10">
+            <div class="column__text">
+                <h2 class="column__text__title">重ねる月日と共に<br> 「自分のもの」になっていく</h2>
+                <p class="column__text__inner">古時計や木造建築など、<br class="pc-only">使い込まれたものだけが持つ、優しくも濃厚な存在感。<br class="pc-only">時を重ねることでしか得られない魅力が、本物にはあります。<br class="pc-only">Pitoroの製品はその「時」、<br class="pc-only">つまりあなたの人生を刻むための「相棒」を作ることが<br class="pc-only">コンセプトとなっています。</p>
+            </div>
+            <!-- Swiper -->
+            <div class="swiper-container swiper-container__fade">
+                <div class="swiper-wrapper swiper-wrapper__fade">
+                    <div class="swiper-slide swiper-slide__fade"><img class="swiper-slide-img" src="/img/slider01.png" alt="作品画像"></div>
+                    <div class="swiper-slide swiper-slide__fade"><img class="swiper-slide-img" src="/img/slider03.png" alt="作品画像"></div>
+                    <div class="swiper-slide swiper-slide__fade"><img class="swiper-slide-img" src="/img/slider04.png" alt="作品画像"></div>
+                </div>
+            </div>    
+        </article>
+        <section class="about" id="about">
+            <div class="about__inner animate" data-animate="fadeIn" data-duration="2.0s" data-delay="0.5s" data-offset="10">
+                <p class="category-title">ABOUT</p>
+                <h2 class="lead">革職人 Pitoro —ピトロ—</h2>
+                <p class="text">Pitoroは、京都を拠点に活動する革職人です。この文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文</p>
+                <p class="readmore">>> READ MORE</p>
+                <p class="text more-text">Pitoroは、京都を拠点に活動する革職人です。 
+                        この文章はダミーですこの文章はダミーですこの文章はダミーです
+                        この文章はダミーですこの文章はダミーですこの文章はダミーです
+                        この文章はダミーですこの文章はダミーですこの文章はダミーです
+                        この文章はダミーですこの文章はダミーですこの文章はダミーです
+                        この文章はダミーです。 </p>
+            </div>
+            <figure class="about__img"><img class="about__img-wide" src="/img/pitoro_craftsman.png" alt="Pitoroの職人"></figure>
+        </section>
+        <div class="btn-container">
+            <a class="btn" href="https://pitoro.base.shop/" target="_blank"><img class="icon" src="/img/store-icon.png" alt="ストアアイコン">STORE</a>
+            <a class="btn" href="#order"><img class="icon" src="/img/order-icon.png" alt="オーダーメイドアイコン">ORDER MADE</a>
+        </div>
+        <article class="column reverse animate" data-animate="fadeIn" data-duration="2.0s" data-delay="0.5s" data-offset="10">        
+            <div class="column__text column__text-reverse">
+                <h2 class="column__text__title">目指すのは、<br> あなたの人生の名脇役</h2>
+                <p class="column__text__inner">丈夫でなければ長くは使えない。<br class="pc-only">外見がよくても、使いにくくては意味がない。<br class="pc-only">全ての製品に対して、最も意識しているのがこの二点です。<br class="pc-only"> 少しでも長く糸が切れないように。<br class="pc-only">少しでも優しく、使う人の手に馴染むように。<br class="pc-only">手作業による様々な工程の全ては、そこへ集約していきます。</p>
+            </div>
+            <!-- Swiper -->
+            <div class="swiper-container swiper-container__fade">
+                <div class="swiper-wrapper swiper-wrapper__fade">
+                    <div class="swiper-slide swiper-slide__fade"><img class="swiper-slide-img" src="/img/slider05.png" alt="作品画像"></div>
+                    <div class="swiper-slide swiper-slide__fade"><img class="swiper-slide-img" src="/img/slider06.png" alt="作品画像"></div>
+                    <div class="swiper-slide swiper-slide__fade"><img class="swiper-slide-img" src="/img/slider07.png" alt="作品画像"></div>
+                </div>
+            </div>             
+        </article>
+        <section class="order" id="order">
+            <div class="order__inner animate" data-animate="slideInLeft" data-duration="2.0s" data-delay="0.5s" data-offset="00">
+                <p class="category-title">FULL ORDER MADE</p>
+                <h2 class="lead">0 から生み出すオーダーメイド</h2>
+                <p class="text">Pitoroではフルオーダーメイドの<br class="sp-only">ご依頼も承っております。</p>
+                <div class="order__inner__item">
+                    <h3 class="order__inner__item__title">打ち合わせ</h3>
+                    <p class="order__inner__item__text">お客様の頭の中にあるイメージを元に、パーツ及び糸の色など細部まで決めていきます。代金は前払いです。</p>
+                    <div class="arrow"></div>
+                </div>
+                <div class="order__inner__item">
+                    <h3 class="order__inner__item__title">制作期間</h3>
+                    <p class="order__inner__item__text">資材が揃い次第、製作に取りかかります。納期の目安はおおよそ1～2ヶ月程度です。</p>
+                    <div class="arrow"></div>
+                </div>
+                <div class="order__inner__item">
+                    <h3 class="order__inner__item__title">仕上げ・完成</h3>
+                    <p class="order__inner__item__text">仕上げに汚れ落とし及び保革クリームを塗り込み、完成です。</p>
+                    <div class="arrow"></div>
+                </div>
+                <div class="order__inner__item">
+                    <h3 class="order__inner__item__title">納品</h3>
+                    <p class="order__inner__item__text">郵送及び店頭での直接お渡しとなります。オーダー時にご希望の納品方法をお申し付けください。</p>
+                </div>
+            </div>
+            <a class="contact-btn" href="#contact">CONTACT</a>
+        </section>
+        <section class="backside animate" id="backSide" data-animate="fadeIn" data-duration="2.0s" data-delay="0.1s" data-offset="100">
+            <div class="backside__inner">
+                <p class="category-title">THE BACK SIDE</p>
+                <h2 class="lead">Pitoroのものづくりの裏側</h2>
+                <p class="text">いいものをつくるために、<br class="sp-only">全ての工程において手を抜きません。<br class="sp-only">作業風景を一部抜粋してご紹介します。</p>    
+            </div>
+            <!-- Swiper -->
+            <div class="swiper-container swiper-container__backside">
+                <div class="swiper-wrapper swiper-wrapper__backside">
+                    <div class="swiper-slide swiper-slide__backside">
+                        <div class="slide__item">
+                            <p class="img-wrapper"><img class="slide__item__img" src="/img/saidan.png" alt=""></p>
+                            <div class="slide__item__text">
+                                <h4 class="slide__item__text-title">断裁</h4>
+                                <p class="slide__item__text-inner">革の繊維や傷を見極め、パーツ毎に適した部位を革包丁で切り出します。しっかりと研いだ革包丁を用い、型紙と寸分の狂いもなく切り出すことが肝要です。</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide swiper-slide__backside">
+                        <div class="slide__item">
+                            <p class="img-wrapper"><img class="slide__item__img" src="/img/suki.png" alt=""></p>
+                            <div class="slide__item__text">
+                            <h4 class="slide__item__text-title">漉（す）き</h4>
+                            <p class="slide__item__text-inner">縫い重なる部分や曲げる部分など、完成した姿を考慮しながら手作業にて革の厚みを調整していきます。スッキリした上品な仕上げを目指しつつ、薄くなりすぎて強度が落ちないよう最適な厚みを考えながら行う繊細な作業です。</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide swiper-slide__backside">
+                        <div class="slide__item">
+                            <p class="img-wrapper"><img class="slide__item__img" src="/img/nui.png" alt=""></p>
+                            <div class="slide__item__text">
+                            <h4 class="slide__item__text-title">手縫い</h4>
+                            <p class="slide__item__text-inner">「本縫い」と呼ばれる、一本の糸に二つの針を取り付ける技法で縫い進めていきます。ミシンのような上糸下糸の概念はなく、互いの糸がもう片方の糸を締め付けているため、縫い目がひとつ切れたとしてもそれ以上解れることがない頑丈な縫製です。修理の際に簡単に縫い直せるのもポイント。</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide swiper-slide__backside">
+                        <div class="slide__item">
+                            <p class="img-wrapper"><img class="slide__item__img" src="/img/kobamigaki.png" alt=""></p>
+                            <div class="slide__item__text">
+                            <h4 class="slide__item__text-title">コバ磨き</h4>
+                            <p class="slide__item__text-inner">全ての切り口（コバ）の段差をとことん無くし、革の繊維を磨き込んで仕上げています。とても手間のかかる工程ですが、この仕上げをしっかりと施すことにより何年経っても艶を保ち、長く綺麗に使える物になります。</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Add Pagination -->
+        <div class="swiper-pagination"></div>
+            <!-- Add Arrows -->
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        
+        </section>
+        <div class="message" id="message">
+            <div class="message__inner">
+                <p class="category-title">MESSAGE</p>
+                <p class="text">物を大切に長く使うということは、<br class="pc-only">自分の人生を丁寧に生きることにつながると考えています。<br class="pc-only">擦り傷も汚れも含めて自分の生きた軌跡として、<br class="pc-only">末長くお使いいただければ幸いです。 <br class="pc-only">また、Pitoroでは、時と共に深みが増すものづくりのために、<br class="pc-only">お手入れ・修理対応などのメンテナンスも行っております。<br> 革製品でお困りごとがございましたらお気軽にご相談ください。</p>    
+            </div>
+            <figure><img class="logo-black" src="/img/logo-black.png" alt="ロゴ"></figure>
+        </div>
+        <div class="contact" id="contact">
+            <p class="category-title">CONTACT</p>
+            <div id="formWrapper">
+                <form action="/confirmation.php" method="post" name="myForm" class="contact__inner">
+                    <div class="contact__inner__item">
+                        <p class="item-name"><span class="must">※</span>お名前</p>
+                        <input type="text" class="contact__name input" name="name" autocomplete="name" required>
+                    </div>
+                    <div class="contact__inner__item">
+                        <p class="item-name"><span class="must">※</span>メールアドレス</p>
+                        <input type="email" class="contact__email input" name="mail" autocomplete="email" required>
+                    </div>
+                    <div class="contact__inner__item">
+                        <p class="item-name">都道府県</p>
+                        <input type="text" class="contact__area input"  placeholder="都道府県" name="area">
+                    </div>
+                    <div class="contact__inner__item">
+                        <p class="item-name"><span class="must">※</span>お問い合わせ内容</p>
+                        <select class="contact__purpose input" placeholder="お問い合わせ内容" name="purpose" required>
+                            <option>オーダーメイドのご相談</option>
+                            <option>修理に関するご相談</option>
+                            <option>商品に関するお問い合わせ</option>
+                            <option>その他</option>
+                        </select>
+                    </div>
+                    <div class="contact__inner__item">
+                        <p class="item-name"><span class="must">※</span>お問い合わせ本文</p>
+                        <textarea name="inquiry"  class="contact__content input" required></textarea>
+                    </div>
+                    <input class="contact-btn-submit" type="submit" name="submit"　value="送　　信">
+                </form>
+                <p class="attention">・3日営業日以内にご返信いたします。万が一返信がない場合は、お手数ですが再度お問い合わせください。<br>・お見積もりの場合は1週間程度お日にちをいただくことがございます。</p>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <small>&copy; Pitoro. All rights reserved.</small>
+    </footer>
+<!-- Loader -->
+<script>
+    jQuery(window).on('load', function() {
+        jQuery('.loader').hide();
+    });
+</script>
+
+<!--- Drawer Menu -->
+<script>
+    jQuery(function($) {
+        var $nav   = $('#navArea');
+        var $btn   = $('.toggle_btn');
+        var $mask  = $('#mask');
+        var open   = 'open'; // class
+        // menu open close
+        $btn.on( 'click', function() {
+            if ( ! $nav.hasClass( open ) ) {
+            $nav.addClass( open );
+            } else {
+            $nav.removeClass( open );
+            };
+        });
+        // mask close
+        $mask.on('click', function() {
+            $nav.removeClass( open );
+
+        });
+    } );
+</script>
+
+<!-- Initialize Swiper -->
+<script>
+  var swiper = new Swiper('.swiper-container__fade', {
+    effect: 'fade',
+    speed:3000,
+    autoplay: {
+        delay: 3000,
+      },
+  });
+</script>
+<script>
+    var swiper = new Swiper('.swiper-container__backside', {
+        slidesPerView: "auto",
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: false,
+        },
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+</script>
+
+<!-- read more -->
+<script>
+    jQuery(function($){
+      $(".readmore").on('click',function(){
+        $(".more-text").slideDown("slow");
+        $(".more-text").css("display","block");
+        $(".readmore").slideUp("slow");
+      });
+    });
+  </script>
+  <!-- スムーススクロール -->
+<script>
+    $(function($){
+    $(".to-top-btn").on('click',function(){
+        $('html,body').animate({ 
+        'scrollTop': 0 
+        }, 'slow');
+    });
+    
+    $(".contact-btn, .btn, .nav__inner__list__item a").on('click',function(){
+        var id = $(this).attr("href");
+        var position = $(id).offset().top
+        $("html,body").animate({
+            "scrollTop":position
+        },"slow");
+    
+        });
+    
+        jQuery(window).on("scroll", function($) {
+        if (jQuery(this).scrollTop() > 410) {
+            jQuery(".link,.to-top-btn,.toggle_btn").fadeIn(500);
+        } else {
+            jQuery(".link,.to-top-btn,.toggle_btn").fadeOut(500);
+        }
+    });
+        });
+</script> 
+
+<!-- scrolla -->
+    <script>
+    $('.animate').scrolla({
+        once: true,
+        mobile: false
+    });
+    </script>     
+  
+</body>
+</html>
